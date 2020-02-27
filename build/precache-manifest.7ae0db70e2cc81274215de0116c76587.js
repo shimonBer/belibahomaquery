8ec77840d18ecaff61dcbd04c46d6d3f20225908
shimonBer <shimon.ber@gmail.com>
2020-02-27 16:43:38 +0200

    first commit

---
+++ build/precache-manifest.7ae0db70e2cc81274215de0116c76587.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685efccfb6ad5c02a7d322bacf2bf9f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c01187c2d27adc58a487",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4abce9f0e3c84f0cf30f",
+    "url": "/static/js/2.d383a27c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.d383a27c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01187c2d27adc58a487",
+    "url": "/static/js/main.5878f1c7.chunk.js"
+  },
+  {
+    "revision": "b64e81c79d066f37dd59",
+    "url": "/static/js/runtime-main.4281e017.js"
+  }
+]);
